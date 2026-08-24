462d18840d006e520bc044bd6ad26a42ebc84f64
stian borge <Stian.h.borge@gmail.com>
2026-08-24 15:19:51 +0200

    Initialize JobTrack backend API

---
+++ Backend/JobTrack.Api/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobTrack.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetHealth()
+    {
+        return Ok(new
+        {
+            status = "Healthy",
+            application = "JobTrack API",
+            timestamp = DateTime.UtcNow
+        });
+    }
+}
